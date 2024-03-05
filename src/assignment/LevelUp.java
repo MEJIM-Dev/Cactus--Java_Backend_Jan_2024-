7867eccf728498521e3204c1d43ee9d5700c1baa
Saviour Umoeka <saviourumoeka@gmail.com>
2024-03-05 12:33:45 +0100

    Problem solving approach

---
+++ src/assignment/LevelUp.java
@@ -0,0 +1,71 @@
+package assignment;
+
+public class LevelUp {
+
+    public static void main(String[] args) {
+
+        /*Input: Scores/Points, Minimum Rank
+
+        Step 1: collect and store input
+        score - int[] or  List<int>
+        minimumRank - int
+
+        count - int
+
+        Step 2: Sort the scores
+
+        Step 3: Count the player above the minimum rank
+
+        Step 4: return the count
+
+
+        //Output: Count of players
+
+         */
+
+    }
+}
+
+/* 5,6,3,1,6,8,9 and the minimum rank to level up is 3rd
+
+Sort: 9,8,6,6,5,3,1
+
+  1st, 2nd, 3rd, 3rd
+
+  4
+*
+* */
+
+/*
+* Given five positive integers,
+* find the minimum and maximum values that can be calculated
+* by summing exactly four of the five integers.
+* Then print the respective minimum and maximum values as
+* a single line of two space-separated long integers.
+*
+* 1,3,4,6,3
+*
+* first sum should the minimum = 1 + 3 + 3 + 4 = 11
+*
+* Second sum should be the maximum = 6 + 4 + 3 +3 = 16
+*
+* Output: 11  16
+*
+*
+* */
+/*
+Input: Collect Input, 5 Fives Integers
+
+Process:
+Step 1: Check for Max and min sum
+1,3,4,6,3
+For Max sum four highest values
+Find four highest values and sum them
+
+For Min sum four lowest values
+Find four lowest values and sum them
+
+Output: Print minimum and maximum values
+* */
+
+
